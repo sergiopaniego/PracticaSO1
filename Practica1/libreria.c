fd03d52c313a512081ea8bdfcec3684fa77e3ca3
sergiopaniego <sergiopaniegoblanco@gmail.com>
2016-11-02 18:58:22 +0100

    Memoria liberada en longlines

--- Practica1/libreria.c
+++ Practica1/libreria.c
@@ -58,11 +58,77 @@ int tail(int N) {
     
 }
 
+void qs(char** lista,int limite_izq,int limite_der)
+{
+    int izq,der,pivote;
+    char* temporal;
+
+    izq=limite_izq;
+    der = limite_der;
+    pivote = strlen(lista[(izq+der)/2]);
+
+    do{
+        while(strlen(lista[izq])<pivote && izq<limite_der)izq++;
+        while(pivote<strlen(lista[der]) && der > limite_izq)der--;
+        if(izq >=der)
+        {
+            temporal= lista[izq];
+            lista[izq]=lista[der];
+            lista[der]=temporal;
+            izq++;
+            der--;
+
+        }
+
+    }while(izq >=der);
+    if(limite_izq<der){qs(lista,limite_izq,der);}
+    if(limite_der>izq){qs(lista,izq,limite_der);}
+}
+
+void quicksort(char** lista,int n)
+{
+    qs(lista,0,n-1);
+}
+
 int longlines(int N) {
-    int lineas;
+    int nlineas;
     char buf[1024];
-    int cuenta = 0;
-    
+    int j=0;
+    int linmin=0;
     //Asignamos las lineas a leer  
-    lineas=N;
+    nlineas=N;
+    //Reservamos espacio para las N líneas
+    char** lineas=(char**) malloc (N*sizeof(char*));
+    for(int i=0;i<N;i++){
+        lineas[i]=(char*) malloc (1024*sizeof(char*));
+    }
+    //Rellenamos el array con las líneas hasta que se vacie el buffer, recorriendolo entero
+    
+    //Primero rellenamos el array hasta que este lleno, luego ordenamos con quicksorts y vamos sustituyendo el último si procede
+    while((fgets(buf,1024,stdin)!=NULL)){
+        if(j<N){
+            strcpy(lineas[j],buf);
+            j++;     
+        }else if(j==N){
+            quicksort(lineas,sizeof(lineas));
+            linmin=sizeof(lineas[N-1]);
+            j++;
+        }else{
+            if(sizeof(buf)>sizeof(lineas[N-1])){
+                strcpy(lineas[N-1],buf);
+                quicksort(lineas,sizeof(lineas));
+                linmin=sizeof(lineas[N-1]);
+            }
+        }
+    }
+    //Imprimimos el resultado por consola
+    for(int k=0; k<N; k++){
+        printf("%s", lineas[k]);
+    }
+    //Liberamos la memoria utilizada
+    for(int i=0; i<N; i++){
+        free(lineas[i]);
+    }
+    free(lineas);
+    return 0;
 }
