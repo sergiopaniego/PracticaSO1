bdf886e196aab1e2881328c636c5aeb9be8e361b
sergio <sergio@sergio-Lenovo-G50-70>
2016-11-04 15:23:22 +0100

    Método longlines funcionalidad acabada (mucha complejidad)

--- Practica1/libreria.c
+++ Practica1/libreria.c
@@ -60,42 +60,10 @@ int tail(int N) {
     
 }
 
-void qs(char** lista,int limite_izq,int limite_der)
-{
-    int izq,der,pivote;
-    char* temporal;
-
-    izq=limite_izq;
-    der = limite_der;
-    pivote = strlen(lista[(izq+der)/2]);
-
-    do{
-        while(strlen(lista[izq])<pivote && izq<limite_der)izq++;
-        while(pivote<strlen(lista[der]) && der > limite_izq)der--;
-        if(izq >=der)
-        {
-            temporal= lista[izq];
-            lista[izq]=lista[der];
-            lista[der]=temporal;
-            izq++;
-            der--;
-
-        }
-
-    }while(izq >=der);
-    if(limite_izq<der){qs(lista,limite_izq,der);}
-    if(limite_der>izq){qs(lista,izq,limite_der);}
-}
-
-void quicksort(char** lista,int n)
-{
-    qs(lista,0,n-1);
-}
-
 int longlines(int N) {
     int nlineas;
     char buf[1024];
-    int j=0;
+    int cont=0;
     int linmin=0;
     //Asignamos las lineas a leer  
     nlineas=N;
@@ -105,25 +73,47 @@ int longlines(int N) {
     for( i=0;i<N;i++){
         lineas[i]=(char*) malloc (1024*sizeof(char*));
     }
+    char* aux=(char*) malloc (1024*sizeof(char*));
     //Rellenamos el array con las líneas hasta que se vacie el buffer, recorriendolo entero
     
     //Primero rellenamos el array hasta que este lleno, luego ordenamos con quicksorts y vamos sustituyendo el último si procede
     while((fgets(buf,1024,stdin)!=NULL)){
-        if(j<N){
-            strcpy(lineas[j],buf);
-            j++;     
-        }else if(j==N){
-            quicksort(lineas,sizeof(lineas));
-            linmin=sizeof(lineas[N-1]);
-            j++;
+        //Metemos las primeras N lineas
+        if(cont<N){
+            strcpy(lineas[cont],buf);
+            cont++;     
         }else{
-            if(sizeof(buf)>sizeof(lineas[N-1])){
-                strcpy(lineas[N-1],buf);
-                quicksort(lineas,sizeof(lineas));
-                linmin=sizeof(lineas[N-1]);
+            //Comprobamos si la linea que leemos es mas larga que la mas corta de las que tenemos
+                if(strlen(buf)>strlen(lineas[N-1])){
+                    strcpy(lineas[N-1], buf);
+                }
+                //Reordenamos las lineas
+                int i, j;
+            for(i=0; i<N-1; i++){
+                for(j=0; j<N-1; j++){
+                    if(strlen(lineas[j])<strlen(lineas[j+1])){
+                        strcpy(aux, lineas[j]);
+                        strcpy(lineas[j], lineas[j+1]);
+                        strcpy(lineas[j+1], aux);                     
+                    }
+                }
             }
+            
         }
     }
+    //Volvemos a ordenar las lineas por si se introducen mas lineas de las que disponemos
+    int x, y;
+    for(x=0; x<N-1; x++){
+                for(y=0; y<N-1; y++){
+                    if(strlen(lineas[y])<strlen(lineas[y+1])){
+                        strcpy(aux, lineas[y]);
+                        strcpy(lineas[y], lineas[y+1]);
+                        strcpy(lineas[y+1], aux);                     
+                    }
+                }
+            }
+    
+    free(aux);
     //Imprimimos el resultado por consola
     int k;
     for( k=0; k<N; k++){
